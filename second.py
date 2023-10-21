077fb3378fbeab3dd09bafbc82c5aa19bee35d18
jatanolu <jacobrjean@gmail.com>
2023-10-22 00:47:28 +0200

    if and while

---
+++ second.py
@@ -0,0 +1,24 @@
+name = 'Alice'
+
+if name == 'Alice':
+    print('Hi Alice')
+print("Done")
+
+password = 'swordfish'
+if password == 'swordfish':
+    print("Access granted.")
+else:
+    print("Wrong password.")
+
+name = 'Bob'
+age = 3000
+if name == 'Alice':
+    print("Hi Alice")
+elif age < 12:
+    print("You are not Alice, kiddo.")
+elif age >  2000:
+    print('Unlike you, Alice is not an undead, immortal vampire!')
+elif age > 100:
+    print('You are not Alice, grannie.')
+
+print(bool(0.0))
