34f1a5d35ebcd2c4d34bfc2912545739bf71ccbc
jatanolu <jacobrjean@gmail.com>
2023-10-22 15:14:40 +0200

    Some more basics

---
+++ fourth.py
@@ -0,0 +1,18 @@
+def hello():
+    print("hey")
+    print("howdy")
+    print("hi")
+
+hello()
+
+def hiPerson(name):
+    print("HI " + name)
+
+hiPerson("Bob")
+hiPerson("Alice")
+
+def plusOne(number):
+    return number + 1
+
+added = plusOne(4)
+print(added)
--- third.py
+++ third.py
@@ -8,3 +8,13 @@ while name != 'your name':
     print('Please type your name.')
     name = input()
 print('Thank you!')
+
+total = 0
+for num in range(101):
+    total = total + num
+print(total)
+
+import sys
+print("hello")
+sys.exit()
+print("not")
